c885f1fce4d791c19894b14ff5b58626e0999595
Colin Bookman <bookman@google.com>
2019-02-18 23:40:54 -0800

    Got writes working, will work on reads

--- main.go
+++ main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"time"
+	"cloud.google.com/go/bigtable"
 	"flag"
+	"github.com/satori/go.uuid"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
 	"log"
 	"math/rand"
-	"cloud.google.com/go/bigtable"
-	"golang.org/x/net/context"
-	"github.com/satori/go.uuid"
 )
 
 var (
-	project   =flag.String("project", "snap-tests-217018", "gcp project")
+	project   = flag.String("project", "snap-tests-217018", "gcp project")
 	instance  = flag.String("instance", "testing", "gcp instance")
-	tableName = flag.String("tbl", "test", "table name")
+	tableName = flag.String("tbl", "", "table name")
+	populate  = flag.Bool("populate", true, "populate test data")
 )
 
 func populateNRows(n int, vs int, btc *bigtable.Client) error {
+	log.Print("Generating mutations")
 	rowKeys := make([]string, n, n)
 	muts := make([]*bigtable.Mutation, n, n)
 	for i := 0; i < n; i++ {
-		uuid, err := uuid.NewV4()
+		k, err := uuid.NewV4()
 		if err != nil {
 			return err
 		}
@@ -29,49 +34,116 @@ func populateNRows(n int, vs int, btc *bigtable.Client) error {
 		rand.Read(value)
 		mut.Set("family", "column", bigtable.Now(), value)
 
-		rowKeys[i] = uuid.String()
+		rowKeys[i] = k.String()
 		muts[i] = mut
 	}
 
-	// TODO(bookman): Add saving of bulk rows
+	log.Print("apply mutation")
+	tbl := btc.Open(*tableName)
+	mutErrs, applyErr := tbl.ApplyBulk(context.Background(), rowKeys, muts)
+	if applyErr != nil {
+		return applyErr
+	}
+	for _, mutErr := range mutErrs {
+		if mutErr != nil {
+			return mutErr
+		}
+	}
 	return nil
 }
 
-func createTestData(btac *bigtable.AdminClient, btc *bigtable.Client) (*bigtable.Table, error) {
-	// re-create table if already exists
+func populateTable(btac *bigtable.AdminClient, btc *bigtable.Client) error {
+	log.Print("Populating table")
+
+	// clean up table if already exists
+	log.Print("Deleting old table if exists")
 	btac.DeleteTable(context.Background(), *tableName)
+
+	log.Print("Creating new table")
 	if err := btac.CreateTable(context.Background(), *tableName); err != nil {
-		return nil, err
+		return err
 	}
 
-	tbl := btc.Open(*tableName)
+	log.Print("Creating Column Family")
+	if err := btac.CreateColumnFamily(context.Background(), *tableName, "family"); err != nil {
+		return err
+	}
+
 	// add a bunch of rows
-	for i := 0; i < 50; i++ {
-		if err := populateNRows(100000, 2*1024, btc); err != nil {
-			return nil, err
+	log.Print("Adding rows")
+	batches := 200
+	rows := 5000000
+	rowsPerBatch := rows / 200
+	workers := 25
+	bytesPerRow := 2*1024
+	jobs := make(chan int, batches)
+	results := make(chan error, batches)
+
+	worker := func() {
+		for j := range jobs {
+			results <- populateNRows(j, bytesPerRow, btc)
 		}
 	}
-	return tbl, nil
+
+	for w := 0; w < workers; w++ {
+		go worker()
+	}
+
+	start := time.Now()
+	for b := 0; b < batches; b++ {
+		jobs <- rowsPerBatch
+	}
+	close(jobs)
+
+	for r := 0; r < batches; r++ {
+		if err := <-results; err != nil {
+			return err
+		}
+
+	}
+	elapsed := time.Now().Sub(start)
+	fmt.Printf("Writing %d rows for each batches %d times, with %d workers took %d seconds, or %d writes per second\n",
+		  rowsPerBatch, batches, workers, elapsed.Seconds(), rows/int(elapsed.Seconds()))
+	return nil
 }
 
 func main() {
+	// Adding line #s to log output
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	flag.Parse()
 	log.Print("Starting up")
 
-	btac, err := bigtable.NewAdminClient(context.Background(), *project, *instance)
-	if err != nil {
-		log.Fatal(err)
-	}
-	btc, err := bigtable.NewClient(context.Background(), *project, *instance)
+	co := option.WithCredentialsFile("service_account.json")
+
+	log.Print("Creating admin client")
+	btac, err := bigtable.NewAdminClient(context.Background(), *project, *instance, co)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tbl, err := createTestData(btac, btc)
+	log.Print("Creating normal client")
+	btc, err := bigtable.NewClient(context.Background(), *project, *instance, co)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	if len(*tableName) == 0 {
+		k, err := uuid.NewV4()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ks := k.String()
+		tableName = &ks
+	}
+	if *populate {
+		if err := populateTable(btac, btc); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	tbl := btc.Open(*tableName)
 	readOptions := []bigtable.ReadOption{
 		bigtable.RowFilter(bigtable.LatestNFilter(1)),
 	}
